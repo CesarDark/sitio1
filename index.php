d9f5f88d23a811da53495b49281303879d5731ad
CesarDark <dark15crm@hotmail.com>
2019-02-23 18:39:25 -0600

    UPD: sitio1 index act 2

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
 		<title> ¡Bienvenido a sitio1! </title>
 	</head>
 	<body>
-		<?php 
-			echo "Mensaje con PHP"
-		?>
 		<h1> El virtual host sitio1 está funcionando </h1>
 		<h2> Mensaje con HTML </h2>
+
+		<?php 
+			echo "Mensaje con PHP";
+		?>	
 	</body>
 </html>
