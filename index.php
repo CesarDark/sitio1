ffeed08db0c7d0cee4d2de360496b420eff6d782
CesarDark <dark15crm@hotmail.com>
2019-02-23 18:33:49 -0600

    UPD: sitio1 index

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 		<title> ¡Bienvenido a sitio1! </title>
 	</head>
 	<body>
+		<?php 
+			echo "Mensaje con PHP"
+		?>
 		<h1> El virtual host sitio1 está funcionando </h1>
+		<h2> Mensaje con HTML </h2>
 	</body>
 </html>
