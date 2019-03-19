465657f9adb1b8710e856ca7b8c4793b95d8fdee
CesarDark <dark15crm@hotmail.com>
2019-03-19 10:51:33 -0600

    UPD: index

--- index.php
+++ index.php
@@ -6,8 +6,12 @@
 		<h1> El virtual host sitio1 está funcionando </h1>
 		<h2> Mensaje con HTML </h2>
 
-		<?php 
+		<?php
 			echo "Mensaje con PHP";
-		?>	
+			echo "<br/><br/>";
+		?>
 	</body>
+	<footer>
+		<a href="Funciones.php"> Tarea #7 Funciones</a>
+	</footer>
 </html>
